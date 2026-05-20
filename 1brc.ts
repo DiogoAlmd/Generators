4082b45c3bcf738910bd73e5faf0df5c4606061e
diogoalmd <diogo.meida@gmail.com>
2026-05-19 21:42:03 -0300

    refactor: converting to json

--- 1brc.ts
+++ 1brc.ts
@@ -1,4 +1,5 @@
 import { createReadStream } from "node:fs";
+import { setTimeout } from "node:timers/promises";
 
 async function* readLines(file_path: string) {
 	let leftover = "";
@@ -14,32 +15,15 @@ async function* readLines(file_path: string) {
 	if (leftover) yield leftover;
 }
 
-const stats = new Map<
-	string,
-	{ sum: number; count: number; min: number; max: number }
->();
-
 const memBefore = process.memoryUsage().heapUsed;
 for await (const line of readLines("./1brc/measurements.txt")) {
 	const sep = line.indexOf(";");
 	const city = line.slice(0, sep);
 	const temp = parseFloat(line.slice(sep + 1));
 
-	const entry = stats.get(city);
-	if (entry) {
-		entry.sum += temp;
-		entry.count++;
-		if (temp < entry.min) entry.min = temp;
-		if (temp > entry.max) entry.max = temp;
-	} else {
-		stats.set(city, { sum: temp, count: 1, min: temp, max: temp });
-	}
-}
-
-for (const [city, { sum, count, min, max }] of stats) {
-	console.log(
-		`${city}: min=${min}, max=${max}, mean=${(sum / count).toFixed(1)}`,
-	);
+	const json = JSON.stringify({ city, temp });
+	console.log(json);
+	await setTimeout(100);
 }
 
 const memAfter = process.memoryUsage().heapUsed;
